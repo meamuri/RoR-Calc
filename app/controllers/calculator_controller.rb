e0900f9f8f83d3a8dc74d1d7035ba4040797af4d
meamuri <meammf@yandex.ru>
2016-12-18 14:59:29 +0300

    now i can pass params to view

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,34 @@
 class CalculatorController < ApplicationController
   def index
   end
+
+  def form_add
+    @type = 'add'
+    render :form
+  end
+
+  def form_sub
+    render :form
+  end
+
+  def form_div
+    render :form
+  end
+
+  def form_mult
+    render :form
+  end
+
+  def form_sin
+    render :form
+  end
+
+  def form_pow
+    render :form
+  end
+
+  def form_sqrt
+    render :form
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get 'calculator/index'
 
-  root 'calculator#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'calculator#index'
+  get 'form_add', to: 'calculator#form_add'
+  get 'form_sub', to: 'calculator#form_sub'
+  get 'form_div', to: 'calculator#form_div'
+  get 'form_mult', to: 'calculator#form_mult'
+  get 'form_sin', to: 'calculator#form_sin'
+  get 'form_pow', to: 'calculator#form_pow'
+  get 'form_sqrt', to: 'calculator#form_sqrt'
 end
